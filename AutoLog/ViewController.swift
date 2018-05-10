99dc0550fc0cfecc45cfd6d8b4ea723278c89c30
Milen Halachev <milenski88@gmail.com>
2018-05-11 00:13:04 +0300

    Initial Commit

---
+++ AutoLog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLog
+//
+//  Created by Milen Halachev on 11.05.18.
+//  Copyright © 2018 KoCMoHaBTa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
