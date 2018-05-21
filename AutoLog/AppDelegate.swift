076ce64e93708d40d02e27245319c8854ff26d51
Milen Halachev <milenski88@gmail.com>
2018-05-22 00:59:17 +0300

    - ViewController - added testLocalNotification method that demonstrates the use of local notifications - AppDelegate - now requests authorization for notifications and shows foreground notifications - AppDelegate - implements local notification upon background app refresh

--- AutoLog/AppDelegate.swift
+++ AutoLog/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (success, error) in
+            
+            
+        }
+        
         return true
     }
 
@@ -40,54 +48,46 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-        // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
     }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "AutoLog")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
+    
+    func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        
+        DispatchQueue.main.async {
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Background Fetch"
+            content.sound = .default()
+            
+            let request = UNNotificationRequest(identifier: "Background Fetch", content: content, trigger: nil)
+            
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: { (error) in
+                
+                DispatchQueue.main.async {
+                    
+                    completionHandler(.newData)
+                }
+            })
         }
     }
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        /*
+         Called when a notification is delivered to a foreground app.
+         */
+        
+        completionHandler([.alert, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        /*
+         This method is callled only when the user taps on an action button of the notification or on the notification itself.
+         */
+        
+        completionHandler()
+    }
+}
--- AutoLog/ViewController.swift
+++ AutoLog/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
+import UserNotifications
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    
+//    let locationManager = CLLocationManager()
+//    let motionManager = CMMotionManager()
+    
+    let motionActivityManager = CMMotionActivityManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.label.text = "0km"
+        
+//        let start = Date(timeIntervalSinceNow: -86400)
+//        let end = Date()
+        
+//        self.motionActivityManager.queryActivityStarting(from: start, to: end, to: .main) { (activities, error) in
+//
+//            print(activities?.count)
+//        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func testLocalNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "test local notification"
+        content.subtitle = "gg"
+        content.body = "vui"
+        content.sound = .default()
+        
+        let request = UNNotificationRequest(identifier: "testLocalNotification", content: content, trigger: nil)
+        
+        UNUserNotificationCenter.current().add(request)
     }
-
-
 }
 
