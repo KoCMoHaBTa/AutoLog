5953f08d434a5d531d023e2f3a2b2925944e2888
Milen Halachev <milenski88@gmail.com>
2018-05-22 19:00:33 +0300

    - added significant location changes monitoring trough local notifications

--- AutoLog/ViewController.swift
+++ AutoLog/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 import CoreLocation
 import CoreMotion
 import UserNotifications
+import MapKit
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
     
-//    let locationManager = CLLocationManager()
+    let locationManager = CLLocationManager()
 //    let motionManager = CMMotionManager()
     
     let motionActivityManager = CMMotionActivityManager()
@@ -26,13 +27,12 @@ class ViewController: UIViewController {
         
         self.label.text = "0km"
         
-//        let start = Date(timeIntervalSinceNow: -86400)
-//        let end = Date()
+        self.locationManager.delegate = self
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.startMonitoringSignificantLocationChanges()
+//        self.locationManager.activityType = .
+//        self.locationManager.pausesLocationUpdatesAutomatically = false
         
-//        self.motionActivityManager.queryActivityStarting(from: start, to: end, to: .main) { (activities, error) in
-//
-//            print(activities?.count)
-//        }
     }
     
     @IBAction func testLocalNotification() {
@@ -46,6 +46,41 @@ class ViewController: UIViewController {
         let request = UNNotificationRequest(identifier: "testLocalNotification", content: content, trigger: nil)
         
         UNUserNotificationCenter.current().add(request)
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        guard let location = locations.first else {
+            
+            return
+        }
+        
+        var id = UIApplication.shared.beginBackgroundTask(withName: "locationUpdate")
+        
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+            
+            guard let placemark = placemarks?.first else {
+                
+                return
+            }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "didUpdateLocations \(locations.count)"
+            content.body = placemark.description
+            
+            content.sound = .default()
+            
+            let request = UNNotificationRequest(identifier: "didUpdateLocations", content: content, trigger: nil)
+            
+            UNUserNotificationCenter.current().add(request) { error in
+                
+                UIApplication.shared.endBackgroundTask(id)
+                id = UIBackgroundTaskInvalid
+            }
+        }
     }
 }
 
