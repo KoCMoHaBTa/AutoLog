8fdd6fc0391812565fdfad1dcb52d3e8c27f8946
Milen Halachev <milenski88@gmail.com>
2018-05-25 13:28:20 +0300

    - DateRangePickerViewController - now sets dates of pickers upon appear

--- AutoLog/DateRangePickerViewController.swift
+++ AutoLog/DateRangePickerViewController.swift
@@ -14,6 +14,14 @@ class DateRangePickerViewController: UIViewController {
     @IBOutlet weak var fromDatePicker: UIDatePicker!
     @IBOutlet weak var toDatePicker: UIDatePicker!
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
+        self.fromDatePicker.date = Date().addingTimeInterval(-7200)
+        self.toDatePicker.date = Date()
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
         let destination = segue.destination as! ActivitiesViewController
